0de374351d492dc5d807ee80f2f4d8d1f7d318e1
Rodrigo Savage <rodrigosavage@gmail.com>
2018-04-06 00:48:10 +0100

    DF

---
+++ config.py
@@ -0,0 +1,5 @@
+consumer_key = "wjnUvDy0IiOreppGkbKNlTN3I"
+consumer_secret = "3l1OyRzNpZBxpr5TSbswPXQrWw3QwcTMJcyFgFk39WLuAJ5gek"
+
+access_token = "885176366173274112-iVT1LgOwAudkYyyHntsiGQD6p1TAVCp"
+access_token_secret = "WJEuRwyVCuV6V77c63nacXBA5cnVH1lAOfhMqVRXkuupo"
